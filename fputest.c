3ad172618338153d632b7812edc448674e57ff1b
cubelube <114615759+cubelube@users.noreply.github.com>
2024-04-17 13:18:06 -0400

    Update fputest.c
    Note: compile with gcc flags -pthread (required) and -Wno-incompatible-pointer-types (optional but suppresses an annoying error)

--- fputest.c
+++ fputest.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <math.h>
-# include <fenv.h>
-void fputest1();
+#include <fenv.h>
+#include <pthread.h>
+#include <string.h>
 
+void *fputest1(void *arg);
 
-
-void main() {
+int main(void) {
     int choice;
+
     printf("C FPU TEST\n");
     printf("\n");
     printf("1. Start Test\n");
@@ -15,22 +17,29 @@ void main() {
 
     if (choice == 1)
     {
-        fputest1();
-    }
-    
+        pthread_t thread;
+        int thread_create = pthread_create(&thread, NULL, fputest1, NULL);
 
+        if (!thread_create)
+        {
+            pthread_join(thread, NULL);
+            return 0;
+        }
+
+        fprintf("%s\n", strerror(thread_create), stderr);
+        return 1;
+    }
 }
 
-void fputest1() {
+void *fputest1(void *arg) {
     float ftest;
     float ftest2 = 1.000002;
     float ftest3 = 1.000002;
+
     for (int i = 0; i < 10000000; i++)
     {
         ftest = ftest2 * ftest3;
         ftest2 = ftest * ftest3;
         printf("%f\n", ftest);
-        
     }
-    
 }
